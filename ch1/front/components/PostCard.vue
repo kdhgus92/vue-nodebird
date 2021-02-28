1671cf2b418b698d94e8a272e94297d6b8b288fb
Ohhyeun Kwon <kdhgus92@gmail.com>
2021-02-28 19:14:06 +0900

    lecture 5-10

--- ch1/front/components/PostCard.vue
+++ ch1/front/components/PostCard.vue
@@ -1,21 +1,17 @@
 <template>
   <div style="margin-bottom: 20px">
     <v-card>
-      <post-images :images="post.Images || []" />
-      <v-card-title>
-        <h3>
-          <nuxt-link :to="'/user/' + post.id">
-            {{ post.User.nickname }}
-          </nuxt-link>
-        </h3>
-      </v-card-title>
-      <v-card-text>
-        <div>
-          <div>{{ post.content }}</div>
-        </div>
-      </v-card-text>
+      <div v-if="post.RetweetId && post.Retweet">
+        <v-subheader>
+          {{ post.User.nickname }}님이 리트윗하였습니다.
+        </v-subheader>
+        <v-card style="margin: 0 20px">
+          <post-content :post="post.Retweet" />
+        </v-card>
+      </div>
+      <post-content v-else :post="post" />
       <v-card-actions>
-        <v-btn text color="orange">
+        <v-btn text color="orange" @click="onRetweet">
           <v-icon>mdi-twitter-retweet</v-icon>
         </v-btn>
         <v-btn text color="orange" @click="onClickHeart">
@@ -56,11 +52,11 @@
 
 <script>
 import CommentForm from "~/components/CommentForm";
-import PostImages from "~/components/PostImages";
+import PostContent from "~/components/PostContent";
 export default {
   components: {
     CommentForm,
-    PostImages,
+    PostContent,
   },
   props: {
     post: {
